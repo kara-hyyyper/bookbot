5391b5f24175ce9ca8843e519957db8e1a74e060
kara-hyyyper <kara@hyyyper.com>
2025-04-15 17:59:37 +0400

    Make bookbot work for any book

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ from stats import get_word_count
 from stats import get_char_count
 from stats import get_char_count_sorted
 import re
+import sys
 
 # -- VARIABLES ------------------------------
 
-BOOKS_PATH = "./books/"
+if (len(sys.argv)>1):
+    BOOKS_PATH = sys.argv[1]
+else:
+     print("Usage: python3 main.py <path_to_book>")
+     sys.exit(1)
 
 # -- FUNCTIONS ------------------------------
 
-def get_book_text(book_name):
+def get_book_text():
     try:
-        with open(BOOKS_PATH + book_name + ".txt") as book:
+        with open(BOOKS_PATH) as book:
             return book.read()
     except:
             print("Book does not exist.")
@@ -22,12 +27,12 @@ def get_book_text(book_name):
 
 def main():
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {BOOKS_PATH}...")
     print("----------- Word Count ----------")
-    word_count = get_word_count(get_book_text("frankenstein"))
+    word_count = get_word_count(get_book_text())
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
-    dict_of_chars = get_char_count_sorted(get_char_count(get_book_text("frankenstein")))
+    dict_of_chars = get_char_count_sorted(get_char_count(get_book_text()))
     for key, value in dict_of_chars.items():
          if (re.match(r'[^[\W_]+$', key)):
               print(f"{key}: {value}")
